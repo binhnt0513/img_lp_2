a1aeea80cfebc25f4abfdbdf9939c8dd8554837b
BinhNT <binhnt.0513@gmail.com>
2024-11-17 21:07:46 +0700

    fix some ui

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,8 @@
 function toggleMenu() {
   const menuIcon = document.getElementById("menuBtn");
-  const closeIcon = document.getElementById("close");
-  const showIcon = document.getElementById("show");
-  const closeLogo = document.getElementById("logo-close");
-  const showLogo = document.getElementById("logo-show");
   const menu = document.getElementById("sideMenu");
   menuIcon.classList.toggle("change");
   menu.classList.toggle("open");
-  if (menu.classList.contains("open")) {
-    closeIcon.style.display = "none";
-    showIcon.style.display = "block";
-    closeLogo.style.display = "none";
-    showLogo.style.display = "block";
-
-  } else {
-    closeIcon.style.display = "block";
-    showIcon.style.display = "none";
-    closeLogo.style.display = "block";
-    showLogo.style.display = "none";
-  }
 }
 
 function navigateToSection(event) {
