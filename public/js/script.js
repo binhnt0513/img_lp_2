ec26b967ae93a9d180813804978f3db5814b11e3
BinhNT <binhnt.0513@gmail.com>
2024-11-17 07:14:02 +0700

    new lp 202411

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,24 @@
 function toggleMenu() {
   const menuIcon = document.getElementById("menuBtn");
+  const closeIcon = document.getElementById("close");
+  const showIcon = document.getElementById("show");
+  const closeLogo = document.getElementById("logo-close");
+  const showLogo = document.getElementById("logo-show");
   const menu = document.getElementById("sideMenu");
   menuIcon.classList.toggle("change");
   menu.classList.toggle("open");
+  if (menu.classList.contains("open")) {
+    closeIcon.style.display = "none";
+    showIcon.style.display = "block";
+    closeLogo.style.display = "none";
+    showLogo.style.display = "block";
+
+  } else {
+    closeIcon.style.display = "block";
+    showIcon.style.display = "none";
+    closeLogo.style.display = "block";
+    showLogo.style.display = "none";
+  }
 }
 
 function navigateToSection(event) {
@@ -13,7 +29,7 @@ function navigateToSection(event) {
 
   if (targetSection) {
     // Scroll to the target section smoothly
-    targetSection.scrollIntoView({ behavior: "smooth" });
+    targetSection.scrollIntoView({behavior: "smooth"});
 
     // Close the menu after clicking
     toggleMenu();
